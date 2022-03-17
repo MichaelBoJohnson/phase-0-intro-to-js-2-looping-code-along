995c90e2e463f3be687dae4d27903f325fb54acb
Michael Johnson <michaelbojohnson@gmail.com>
2022-03-17 08:15:19 -0500

    commit tree ab0a6f594bad8f604363276ee274a51ada7cd5dc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, event){
+    const newArr = [];
+    for (let i = 0; i < names.length; i++){
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown (number){
+    let i = number;
+    while (i > -1){
+        console.log(i);
+        i--;
+    }
+}
